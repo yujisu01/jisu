47e2431dc45e5ad38181b00ad2f1ccf745ffd9aa
yujisu01 <jssla1@naver.com>
2023-01-01 16:11:53 +0900

    230101

--- src/main/webapp/WEB-INF/views/191_200/main1.js
+++ src/main/webapp/WEB-INF/views/191_200/main1.js
@@ -1,3 +1,23 @@
 /**
  * 1-191. 스크립트로 이미지 로딩하기
  */
+const img1= document.querySelector('#loadImage1');
+const img2= document.querySelector('#loadImage2');
+img1.src= '../image/반란루미.jpg';
+img2.src='../image/반계쿵야.jpg';
+
+
+/**
+	1-192. 이미지 로딩후 작업
+	- 이미지 로딩을 지연시키고 싶을때
+	- onload : 로딩완료 시점의 처리작업 지정
+ */
+
+
+const img = document.querySelector('#onloadImage');
+img.onload = () => {
+  // 이미지 로딩 완료 후 처리 작업
+  img.classList.remove('loading');
+};
+img.src='../../image/양파쿵야.jpg';
+img.classList.add('loading');
---
+++ src/main/webapp/WEB-INF/views/191_200/main2.js
@@ -0,0 +1,28 @@
+
+
+/**
+	1-193. 이미지 로딩 지연시키기
+	- 이미지 로딩후 이미지데이터에 액세스
+ */
+// 이미지 로딩을 지연시키려면 DomContentLoaded 이벤트에서 img 요소 data-src속성의 값을
+// Map에 보관한뒤 요소 속성값 비운다. 
+// src가 아닌 data-src를 사용하는 이유? src속성에 공백값이 들어가면 네트워크 통신이 발생하므로..
+
+// 버튼클릭시 이미지를 로딩하는 샘플
+const srcMap= new Map();
+window.addEventListener('DOMContentLoaded', () => {
+	const imgs= document.querySelectorAll('img');		// 모든 img요소 참조
+	imgs.forEach((img) => {
+		srcMap.set(img, img.dataset.src);		// 각 img요소 data-src속성을 Map에 보관
+		img.removeAttribute('src') 		// 로딩 지연시키기 위해 요소 속성 제거
+	});
+});
+
+const btn =document.querySelector('.loadBtn');
+btn.addEventListener('click', () => {
+	const imgs =document.querySelectorAll('img');		// 모든 img요소 참조
+	imgs.forEach((img) => {
+		const source = srcMap.get(img);		// Map에 보관한 값을 src속성에 대입
+		img.src=source;
+	});
+});
