6af3dbb1bef909dec128d3acf0f03e815fbc77a8
yujisu01 <jssla1@naver.com>
2023-01-05 12:12:21 +0900

    230105

--- src/main/webapp/WEB-INF/views/210_218/main3.js
+++ src/main/webapp/WEB-INF/views/210_218/main3.js
@@ -74,75 +74,90 @@ function cloneToImage(data){
  */
 // a 요소를 사용해서 캔버스요소에서 작업한 그래픽 다운로드 가능. 이미지 데이터는 toDataURL()을 사ㅛㅇ해 Base64문자열로 가져옴
 // 바이너리이므로 Blob객체로 변환함
-const can1= document.querySelector('#originCanvas');
-const con1= can1.getContext('2d');
-const img1 = new Image();
+// 캔버스 요소 참조
+const canvas1 = document.querySelector('#canvas-original');
+// 컨텍스트 가져오기
+const context1 = canvas1.getContext('2d');
+console.log(context1);			// 되지가 않네...
+// Image 인스턴스 생성
+const img = new Image();
+// 이미지 로딩 후 처리
+img.onload = () => {
+  // 컨텍스트로 캔버스에 그리기
+  context1.drawImage(img, 0, 0);
 
-img1.onload = () => {
-	con1.drawImage(img1, 0, 0);
-	
-	const imageData = con1.getImageData(0,0,150,150);
-	const data1 = imageData.data;
-	
-	console.log(imageData, data1);
-	
-	const monoImageData= new ImageData(150,150);
-	
-	const monoArr= monoImageData.data;
-	for(let i=0; i< data1.length/4; i+= 1){
-		const r = data1[i*4+0];
-		const g = data1[i*4+1];
-		const b = data1[i*4+2];
-		const a = data1[i*4+3];
-		
-		const color = (r+g+b)/3;
-		
-		monoArr[i*4+0] = color;
-		monoArr[i*4+1] = color;
-		monoArr[i*4+2] = color;
-		monoArr[i*4+3] = a;
-		
-	}
-	
-	// 캔버스 요소 참조 
-	const can2= document.querySelector('#afterCanvas');
-	const con2= can2.getContext('2d');
-	con2.putImageData(monoImageData,0,0);
+  // 화소 정보 가져오기
+  const imageData = context1.getImageData(0, 0, 150, 150);
+  const data = imageData.data;
+
+  const monoImageData = new ImageData(150, 150);
+
+  const monoArr = monoImageData.data;
+  for (let i = 0; i < data.length / 4; i += 1) {
+    // 화소 정보 가져오기
+    const r = data[i * 4 + 0];
+    const g = data[i * 4 + 1];
+    const b = data[i * 4 + 2];
+    const a = data[i * 4 + 3];
+
+    // 평균값 구하기(연산 간략화를 위함)
+    const color = (r + g + b) / 3;
+
+    // 새로운 배열에 색 지정新しい配列に色を指定
+    monoArr[i * 4 + 0] = color;
+    monoArr[i * 4 + 1] = color;
+    monoArr[i * 4 + 2] = color;
+    monoArr[i * 4 + 3] = a;
+  }
+
+  // 캔버스 요소 참조
+  const canvas2 = document.querySelector('#canvas-effected');
+  // 컨텍스트 가져오기
+  const context2 = canvas2.getContext('2d');
+  // 컨텍스트에 새로운 화소 정보 할당
+  context2.putImageData(monoImageData, 0, 0);
 };
-	// img로딩 시작
-	img1.src='../image/la.jpg';
-	
-	const btnDown= document.querySelector('#btnDownload');
-	btnDown.addEventListener('click', () => {
-		const can2= document.querySelector('#afterCanvas');
-		
-		// 파일형식과 파일명 지정
-		const mimeType= 'image/png';
-		const fileName= 'download.png';
-		
-		// Base64 문자열 가져오기
-		const base64 = can2.toDataURL(mimeType);
-		
-		// base64문자열을 Uint8Arrayㄹ 변환
-		const bin= atob(base64.replace(/^.*,/,''));
-		const buffer= new Uint8Array(bin.length);
-		for(let i=0; i<bin.length; i++){
-			buffer[i]= bin.charCodeAt(i);
-		}
-		
-		// Blob생성
-		const blob= new Blob([buffer.buffer], {type: mimeType}
-		);
-		
-		// 이미지 다운로드
-		if(window.navigator.msSaveBlob){
-			window.navigator.msSaveBlob(blob, fileName);
-		}else if(window.URL && window.URL.createObjectURL) {
-			const a = document.createElement(blob);
-			document.body.appendChild(a);
-			a.click();
-			document.body.removeChild(a);
-		}else{
-			window.open(base64, '_blank');
-		}
-	});
+// 이미지 로딩 시작
+img.src = 'la.jpg';
+
+const btnDownload = document.querySelector('#btnDownload');
+btnDownload.addEventListener('click', () => {
+  // 캔버스 요소 참조
+  const canvas2 = document.querySelector('#canvas-effected');
+
+  // 파일 형식과 파일명 지정
+  const mimeType = 'image/png';
+  const fileName = 'download.png';
+
+  // Base64 문자열 가져오기
+  const base64 = canvas2.toDataURL(mimeType);
+
+  // Base64 문자열을 Uint8Array로 변환
+  const bin = atob(base64.replace(/^.*,/, ''));
+  const buffer = new Uint8Array(bin.length);
+  for (let i = 0; i < bin.length; i++) {
+    buffer[i] = bin.charCodeAt(i);
+  }
+
+  // Blob 생성
+  const blob = new Blob([buffer.buffer], {
+    type: mimeType
+  });
+
+  // 이미지 다운로드
+  if (window.navigator.msSaveBlob) {
+    // for IE
+    window.navigator.msSaveBlob(blob, fileName);
+  } else if (window.URL && window.URL.createObjectURL) {
+    // for Firefox, Chrome, Safari
+    const a = document.createElement('a');
+    a.download = fileName;
+    a.href = window.URL.createObjectURL(blob);
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  } else {
+    // for Other
+    window.open(base64, '_blank');
+  }
+});
