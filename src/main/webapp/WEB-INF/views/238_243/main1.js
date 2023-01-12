2b6096b52e571c02a7bbf103561e79b4eaa399e3
yujisu01 <jssla1@naver.com>
2023-01-12 18:17:47 +0900

    230112

--- src/main/webapp/WEB-INF/views/238_243/main1.js
+++ src/main/webapp/WEB-INF/views/238_243/main1.js
@@ -164,3 +164,30 @@ if('serviceWorker' in navigator) {
 }else{
 	console.log('serviceWorker 미대응');
 }
+
+
+
+/**
+ * 1-243. 푸시알림
+	- 브라우저에서 os고유의 알림창을 사용해 알림을 표시하고 싶을때
+	- Notification.permission : 브라우저의 알림허가여부
+	- Notification.requestPermission() : 알림허가 요청
+	- new Notification(알림타이틀) : 알림표시
+ */
+
+const nBtn = document.querySelector('#nBtn');
+nBtn.addEventListener('click', notify);
+
+function notify(){
+	switch (Notification.permission){		// 브라우저의 알림허가여부
+		case 'default': Notification.requestPermission(); 	// 알림허가 요청 (기본상태)
+			console.log('default');
+			break;
+		case 'granted': new Notification("안녕하세요 yjs01님...");			// 알림허가후 Notification알림
+			console.log('granted');
+			break;
+		case 'denied' : alert('알림거부'); 					// denied 알림거부
+			console.log('denied');
+			break;
+	}
+}
--- src/main/webapp/WEB-INF/views/238_243/main2.js
+++ src/main/webapp/WEB-INF/views/238_243/main2.js
@@ -1,3 +1,24 @@
 /**
- * 
+ * 1-243. 푸시알림
+	- 브라우저에서 os고유의 알림창을 사용해 알림을 표시하고 싶을때
+	- Notification.permission : 브라우저의 알림허가여부
+	- Notification.requestPermission() : 알림허가 요청
+	- new Notification(알림타이틀) : 알림표시
  */
+
+const pBtn = document.querySelector('#pBtn');
+pBtn.addEventListener('click', notify);
+
+function notify(){
+	switch (Notification.permission){		// 브라우저의 알림허가여부
+		case 'default': Notification.requestPermission(); 	// 알림허가 요청 (기본상태)
+			console.log('default');
+			break;
+		case 'granted': new Notification('push');			// 알림허가후 Notification알림
+			console.log('granted');
+			break;
+		case 'denied' : alert('알림거부'); 					// denied 알림거부
+			console.log('denied');
+			break;
+	}
+}
